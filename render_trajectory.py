a16abd0a745d54e68a1a1f4bad81ecfd86f05993
Andrew Miller <andrew.colin.miller@gmail.com>
2011-12-22 16:05:08 -0500

    added args to render trajectory

--- render_trajectory.py
+++ render_trajectory.py
@@ -2,27 +2,34 @@ from boxm2_scene_adaptor import *;
 from vil_adaptor import *;
 from vpgl_adaptor import *;
 import random, os, sys, scene_registry;  
+from optparse import OptionParser
 
 ####################################################### 
 # handle inputs                                       #
 #scene is given as first arg, figure out paths        #
-scene_name = "apartments"                             #
-if len(sys.argv) > 1 :                                #
-  scene_name = sys.argv[1]                            #
-scene_root = scene_registry.scene_root( scene_name ); #
-#######################################################
+parser = OptionParser()
+parser.add_option("-s", "--scene", action="store", type="string", dest="scene", help="specify scene name")
+parser.add_option("-x", "--xmlfile", action="store", type="string", dest="xml", default="uscene.xml", help="scene.xml file name (model/uscene.xml, model_fixed/scene.xml, rscene.xml)")
+parser.add_option("-g", "--gpu",   action="store", type="string", dest="gpu",   default="gpu1", help="specify gpu (gpu0, gpu1, etc)")
+parser.add_option("-m", "--maxFrames", action="store", type="int", dest="maxFrames", default=500, help="max number of frames to render")
+(options, args) = parser.parse_args()
+print options
+print args
 
-#################################
-# Set some update parameters
-model_name = "model_color"
-GPU = "gpu0";
+scene_root = scene_registry.scene_root( options.scene ); #
+model_name = options.xml
+if not os.path.exists(scene_root + "/" + model_name):
+  print "Cannot find file: ", scene_root+"/"+model_name
+  sys.exit(-1) 
+GPU = options.gpu;
+MAX_FRAMES = options.maxFrames
 NI=1280
 NJ=720
 #################################
 
 #should initialize a GPU
 os.chdir(scene_root);
-scene_path = os.getcwd() + "/" + model_name + "/uscene.xml";
+scene_path = os.getcwd() + "/" + model_name;
 scene = boxm2_scene_adaptor(scene_path, GPU);  
 
 #init trajectory 
@@ -37,7 +44,7 @@ if not os.path.exists(trajDir):
 ################# 
 # UPDATE LOOP
 #################
-for x in range(0, 500, 1):
+for x in range(0, MAX_FRAMES, 1):
 
   #render frame
   prcam = trajectory_next(trajectory); 
