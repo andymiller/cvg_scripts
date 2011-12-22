bf89c5a1fa7cf06bfd900259fec0f1fa2e66b069
Andrew Miller <andrew.colin.miller@gmail.com>
2011-12-22 15:55:26 -0500

    made scene registry more flexible

--- scene_registry.py
+++ scene_registry.py
@@ -8,32 +8,41 @@ import os, os.path
 
 #SCENE PATHS
 scene_paths = {}
-scene_paths["apartments"] = "/home/acm/data/visualsfm/apts/"
-scene_paths["hemenways"]  = "/home/acm/data/hemenways/"
-scene_paths["capitol"]    = "/home/acm/data/capitol/"
-scene_paths["downtown"]   = "/home/acm/data/downtown/"
-scene_paths["downtown2"]  = "/home/acm/data/downtown2/"
-scene_paths["biltmore"]   = "/home/acm/data/biltmore/"
-scene_paths["eval06"]     = "/home/acm/data/Eval06/"
-scene_paths["bandh"]      = "/home/acm/data/bandh/"
-scene_paths["mall"]       = "/home/acm/data/mall/"
-scene_paths["ppark"]      = "/home/acm/data/ppark/"
-scene_paths["galactica"]  = "/home/acm/data/galactica/"
+#scene_paths["apartments"] = "/home/acm/data/visualsfm/apts/"
+#scene_paths["hemenways"]  = "/home/acm/data/hemenways/"
+#scene_paths["capitol"]    = "/home/acm/data/capitol/"
+#scene_paths["downtown"]   = "/home/acm/data/downtown/"
+#scene_paths["downtown2"]  = "/home/acm/data/downtown2/"
+#scene_paths["biltmore"]   = "/home/acm/data/biltmore/"
+#scene_paths["eval06"]     = "/home/acm/data/Eval06/"
+#scene_paths["bandh"]      = "/home/acm/data/bandh/"
+#scene_paths["mall"]       = "/home/acm/data/mall/"
+#scene_paths["ppark"]      = "/home/acm/data/ppark/"
+#scene_paths["galactica"]  = "/home/acm/data/galactica/"
+
+data_candidates = [ 
+                    "/data/",
+                    "/data/models",
+                    os.path.expanduser("~/data/"),
+                    os.path.expanduser("~/data/models/")
+                  ]
+
 
 def scene_root(scene_name):
-  if scene_name in scene_paths: 
-    spath = scene_paths[scene_name]
-  else: 
-    spath = "/home/acm/data/" + scene_name + "/"
-  if os.path.exists(spath):
-    return spath
-  else:
-    print "SCENE: ", scene_name, " not found!!!!"
-    print_scenes()
-    sys.exit(-1)
+  for cand in data_candidates:
+    if os.path.exists( cand+"/"+scene_name ):
+      return cand+"/"+scene_name
+  
+  #otherwise no path, throw error
+  print "SCENE: ", scene_name, " not found!!!!"
+  print_scenes()
+  sys.exit(-1)
 
 def print_scenes():
-  print "Confirmed Available scenes (may be more):"
-  for scene, path in enumerate(scene_paths):
-    print scene, " located at ", path
+  for cand in data_candidates:
+    print "Checking for data in: ", cand
+    if os.path.exists(cand):
+      print os.listdir(cand)
+    else:
+      print "  ... ", cand, " not found!"
   
