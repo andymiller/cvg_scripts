75d321e3d3b6a57d8743fe49a52d78585ff7f00f
Andy Miller <andrew.colin.miller@gmail.com>
2011-12-29 17:26:22 -0500

    intensity probe more or less complete

--- intensity_probe.py
+++ intensity_probe.py
@@ -1,10 +1,10 @@
+#!/usr/bin/python
 """ 
 Intensity_Probe - given a directory of images and directory of 
 corresponding cameras, click on a point in the presented image 
 and have the intensities at that point in other images 
 plotted to the left (histogrammed and by image number...)
 """
-
 from boxm2_adaptor import *; 
 from boxm2_scene_adaptor import *;
 from vpgl_adaptor import *; 
@@ -70,11 +70,6 @@ imgList.sort()
 camList.sort()
 assert(len(imgList) == len(camList))
 assert(len(imgList) > 0) 
- 
-#Load camera 
-#pcam = load_perspective_camera(cam_name); 
-#img, ni, nj = load_image(image_filename); 
-#cam  = persp2gen(pcam,ni,nj);
 
 
 """ ImageFrame
@@ -82,8 +77,10 @@ Helper class keeps track of objects associated with an image frame
 """
 class ImageFrame:
 
-  def __init__(self, frame=None, currImg=0, row=0, col=0, label_image=None, image=None, tkpi=None):
-    self.frame=frame
+  def __init__(self, frame=None, label=None, labString=None, currImg=0, row=0, col=0, label_image=None, image=None, tkpi=None):
+    self.frame = frame
+    self.label = label
+    self.labString = labString
     self.label_image = label_image 
     self.currImg = currImg 
     self.row = row
@@ -101,37 +98,24 @@ sunphi  =0.495398;
 class App:
     def __init__(self,master,imgList,camList):
         self.master = master; 
-        self.master.title("3d Point");
-        self.point=[0,0,0];
+        self.master.title("3d Point Intensity Tool");
         
-        #set up plot frame 
-        firstImg = Image.open(imgList[0]); 
-        self.frame = Frame(self.master, height=firstImg.size[1], width=firstImg.size[0]);
-        self.frame.grid_propagate(0)
-        self.frame.pack();
-        self.frame.grid(row=0, column=0, sticky=NW)    
-        
-        #set up images frame (should be 4 or so images)
-        self.frames = []
-        self.numImageFrames = 4
-        for i in range(2):
-          for j in range(2):
-            frame1 = Frame(self.master, height=firstImg.size[1], width=firstImg.size[0], bg='red')
-            frame1.pack();
-            frame1.grid_propagate(0)
-            frame1.grid(row=i, column=j+1, sticky=NW)      
-            imageFrame = ImageFrame(frame1, 0, i, j);
-            self.frames.append(imageFrame) 
-                  
-        #display the first frame 
+        #store imgs/cams
         self.imgList = imgList
         self.camList = camList
-        for iFrame in self.frames:
-          self.displayImage(iFrame)      
-        
-        #set camera
-        self.cam = load_perspective_camera(camList[0])
 
+        #Once a 3d point is generated, it is stored here, 
+        #and all image's UV points stored in allUVs
+        self.point3d = None 
+        self.allUVs = []
+        
+        #############################################
+        #set up plot frame 
+        firstImg = Image.open(imgList[0]); 
+        self.frame = Frame(self.master, height=firstImg.size[1], width=firstImg.size[0], bg='blue');
+        self.frame.grid_propagate(0)
+        self.frame.pack();
+        self.frame.grid(row=0, column=0)    
         # place a graph somewhere here
         self.f = Figure(figsize=(5.0,5.0), dpi=100)
         self.a = self.f.add_subplot(311)
@@ -142,14 +126,50 @@ class App:
         self.canvas.show()
         #self.canvas.mpl_connect('button_press_event', self.get_t);
         self.canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
-        
+
+        #############################################
+        #set up button frame 
+        self.bFrame = Frame(self.master, height=firstImg.size[1], width=firstImg.size[0])
+        self.bFrame.grid_propagate(0)
+        self.bFrame.pack()
+        self.bFrame.grid(row=1, column=0)
         #place a button to generate 3d point, and grab intensities from each image
-        self.button = Button(self.master, text="Generate Point", command=self.point_from_rays)
-        self.button.pack()
-        self.button.grid_propagate(0)
-        self.button.grid(row=1, column=0, sticky=NW)
-        master.mainloop();
+        self.genButton = Button(self.bFrame, text="Generate Point", command=self.point_from_rays)
+        self.genButton.pack(fill=BOTH, expand=1)
+        #button to clear the points
+        self.clearButton = Button(self.bFrame, text="Clear Points", command=self.clear_points)
+        self.clearButton.pack()
+        #label for std dev and mean
+        self.stdLabel = StringVar()
+        Label(self.bFrame, textvariable=self.stdLabel).pack()
+        self.meanLabel = StringVar()
+        Label(self.bFrame, textvariable=self.meanLabel).pack()
+
+        ##############################################        
+        #set up images frames (should be 4 or so images)
+        self.frames = []
+        self.numImageFrames = 4
+        frCount = 0
+        for i in range(2):
+          for j in range(2):
+            labString = StringVar()
+            label = Label(self.master, textvariable=labString)
+            frame1 = LabelFrame(self.master, labelwidget=label, height=firstImg.size[1], width=firstImg.size[0], bg='green')
+            frame1.pack();
+            frame1.grid_propagate(0)
+            frame1.grid(row=i, column=j+1, sticky=NW)      
+            
+            currFrame = (len(self.imgList) / self.numImageFrames) * frCount
+            imageFrame = ImageFrame(frame1, label, labString, currFrame, i, j);
+            self.frames.append(imageFrame) 
+            frCount += 1
+                  
+        #display the first frame 
+        for iFrame in self.frames:
+          self.displayImage(iFrame)      
         
+        #start the gui
+        master.mainloop();
         
     def point_from_rays(self):
       """generate point from frames with clicked pixels"""
@@ -164,24 +184,59 @@ class App:
           cam = load_perspective_camera(self.camList[iFrame.currImg])
           cams.append(cam)
       point = get_3d_from_cams(cams, uvs)
+      self.point3d = point;
           
       # project 3d point into each image, and gather intensities   
       values = []
+      ims    = []
       for idx, img in enumerate(self.imgList):
         cam = load_perspective_camera(self.camList[idx])
         imgPoint = project_point(cam, point[0], point[1], point[2])
+        self.allUVs.append(imgPoint)
         
         #grab float intensity value at this point 
-        img = load_image(img)
-        val = pixel(img, imgPoint)
-        values.append(val)
-        
+        imgView,ni,nj = load_image(img)
+        val = pixel(imgView, imgPoint)
+        if val > 0.0:
+          values.append(val)
+          ims.append(idx)
+          
         #cleanup
-        remove_from_db([img, cam])
-        
-      print "Intensities: ", values
+        remove_from_db([imgView, cam])
       
+      #now that each image has a corresponding make sure the
+      #point is displayed in each image
+      #self.clear_points();
+      #for iFrame in self.frames:
+        #point = self.allUVs[iFrame.currImg];
+        #self.drawBox(iFrame)
 
+      #write mean/std of intensities 
+      self.meanLabel.set("Mean: " + str(numpy.mean(values)) )
+      self.stdLabel.set("Std Dev: " + str(numpy.std(values)) )
+      #plot the intensities by image number      
+      self.f.clf();
+      self.a = self.f.add_subplot(311)
+      self.a.set_xlabel("img #")
+      self.a.set_ylabel("intensity")
+      self.a.plot(ims, values)
+      #plot the histogram of intensities by image number      
+      pdf, bins, patches = plt.hist(values)
+      self.b = self.f.add_subplot(313)
+      self.b.set_xlabel("bin val")
+      self.b.set_ylabel("freq")
+      self.b.hist(values, 15, normed=1, facecolor="green" )
+      self.canvas.show();
+      
+    def clear_points(self):
+      """clear points in each iFrame"""
+      print "clearing each frame of selected points"
+      self.point_3d = None
+      self.allUVs = []
+      for iFrame in self.frames:
+        iFrame.lastClick = None; 
+        self.displayImage(iFrame)
+      
     def displayImage(self, iFrame, img=None):
         """given a frame displays the current image """
         if not img:
@@ -192,11 +247,21 @@ class App:
             img.mode = 'I'
             img = img.point(lambda i:i*(1./256.)).convert("RGB");
           
-        #store tkp image
+        #iframe keeps track of its image
         iFrame.image = img
-        iFrame.tkpi = ImageTk.PhotoImage(img)
+         
+        #if point is generated, gotta draw squares first
+        if self.point3d:
+          point = self.allUVs[iFrame.currImg];
+          self.drawBox(iFrame, point)
+         
+        # store photo image (probably not needed in iFrame)
+        iFrame.tkpi = ImageTk.PhotoImage(img) 
         
-        #create new label 
+        #update frames' label 
+        iFrame.labString.set("img {0}".format(iFrame.currImg))
+        
+        #create new label image
         if iFrame.label_image :
           iFrame.label_image.destroy()
         iFrame.label_image = Label(iFrame.frame, image=iFrame.tkpi)
@@ -225,21 +290,19 @@ class App:
     def runprobe(self,event,iFrame):
         print "Image clicked on frame ", iFrame.row, iFrame.col
         print "    at point", event.x, event.y, " = ", iFrame.image.getpixel( (event.x, event.y) )
-        
-        #store x,y clicked
+        #store x,y clicked and draw 
         iFrame.lastClick = (event.x, event.y)
+        self.drawBox(iFrame, iFrame.lastClick)
+        self.displayImage(iFrame, iFrame.image)
         
-        #draw something here 
+    def drawBox(self, iFrame, point):
         draw = ImageDraw.Draw(iFrame.image)
         imSize = iFrame.image.size
-        p1 = ( max(event.x-5,0), max(event.y-5,0) )
-        p2 = ( min(event.x+5,imSize[0]-1), min(event.y+5, imSize[1]-1) )
+        p1 = ( max(point[0]-5,0), max(point[1]-5,0) )
+        p2 = ( min(point[0]+5,imSize[0]-1), min(point[1]+5, imSize[1]-1) )
         draw.rectangle([p1, p2],  fill="green")
         del draw
-        self.displayImage(iFrame, iFrame.image); 
 
-        #shoot and store this ray 
-        
 #        self.posx=event.x;
 #        self.posy=event.y;
 #        array2d=list();
@@ -260,14 +323,6 @@ class App:
 #        print air_p
 #        print air_p1
 #        print num_observations
-
-
-#        self.f.clf();
-#        self.a = self.f.add_subplot(311)
-#        self.a.set_xlabel("zs")
-#        self.a.set_ylabel("Cubic p")
-#        self.a.plot(tabs_array_1d,surface_p);
-#        self.a.plot(tabs_array_1d,vis_array_1d);
 
 #        self.b = self.f.add_subplot(312)
 #        self.b.set_xlabel("zs")
